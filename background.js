7ce93a40fed05a16c6566ae93494272a4dfe570e
Cara Yung <yungcara@gmail.com>
2019-07-26 19:11:34 -0400

    Start timer when new window is created, instead of when chrome is started

--- background.js
+++ background.js
@@ -1,6 +1,12 @@
 'use strict';
 
-var currentTime = new Date();
-chrome.storage.local.set({startTime: currentTime.getTime()}, function() {
-  console.log("Saved session start time of " + currentTime);
+chrome.windows.onCreated.addListener(function() {
+    storeCurrentTime();
 });
+
+function storeCurrentTime() {
+    var currentTime = new Date();
+    chrome.storage.local.set({startTime: currentTime.getTime()}, function() {
+    console.log("Saved session start time of " + currentTime);
+    });
+}
